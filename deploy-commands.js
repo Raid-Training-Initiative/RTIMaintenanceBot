dfd868908a1a962bea62676ecdf169f4bfa782fc
Stephan Wells <stephanwells@hotmail.com>
2022-01-11 14:13:28 +0100

    Update command registration script to work for both guild and global commands

--- deploy-commands.js
+++ deploy-commands.js
@@ -4,6 +4,15 @@ const { REST } = require('@discordjs/rest');
 const { Routes } = require('discord-api-types/v9');
 const { apiKey, clientId } = require('./Config.json');
 
+let commandType = process.argv[2];
+if (!commandType) {
+	commandType = process.env.COMMANDTYPE;
+}
+if (!commandType) {
+	console.log("No configuration specified");
+	return null;
+}
+
 const commands = [];
 const commandFiles = fs.readdirSync('./dist/src/commands').filter(commandFile => commandFile.endsWith('.js'));
 
@@ -15,6 +24,15 @@ for (const commandFile of commandFiles) {
 
 const rest = new REST({ version: '9' }).setToken(apiKey);
 
-rest.put(Routes.applicationGuildCommands(clientId, "633789692893986856"), { body: commands })
-	.then(() => console.log('Successfully registered application commands.'))
-	.catch(console.error);
+switch (commandType) {
+	case "Guild":
+		rest.put(Routes.applicationGuildCommands(clientId, "633789692893986856"), { body: commands })
+			.then(() => console.log('Successfully registered application guild commands.'))
+			.catch(console.error);
+		break;
+	case "Global":
+		rest.put(Routes.applicationCommands(clientId), { body: commands })
+			.then(() => console.log('Successfully registered application global commands.'))
+			.catch(console.error);
+		break;
+}
